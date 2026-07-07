1b32057ad0416df6eff44e53d0594afc1ba5e610
Vinicius-Faria <Viniciguto1@gmail.com.br>
2026-07-07 10:04:57 -0300

    feat - adicionando CredencialDto.java

---
+++ attendant/src/main/java/br/com/attendant/dto/CredencialDto.java
@@ -0,0 +1,11 @@
+package br.com.attendant.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CredencialDto {
+    private String email;
+    private String senha;
+}
